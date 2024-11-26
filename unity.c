1089f11f084da1780dafdbe2e526acd9237fb6c0
Richard Fine <richardf@unity3d.com>
2024-11-25 23:40:39 -0500

    Routine for reading metafile guid

--- unity.c
+++ unity.c
@@ -5,7 +5,9 @@
 #define USE_THE_REPOSITORY_VARIABLE
 
 #include "git-compat-util.h"
+#include "repository.h"
 #include "attr.h"
+#include "object-store-ll.h"
 
 static struct attr_check *unitymetafile_attr;
 int unity_is_metafile(struct repository *r, const char *path)
@@ -17,4 +19,35 @@ int unity_is_metafile(struct repository *r, const char *path)
 	git_check_attr(r->index, path, unitymetafile_attr);
 
 	return ATTR_TRUE(unitymetafile_attr->items[0].value);
+}
+
+static const char guid_prefix[] = "guid: ";
+static const int prefix_length = sizeof(guid_prefix) - 1;
+
+int unity_read_metafile_guid(struct repository *r, 
+                             struct object_id* oid, 
+                             char result[32])
+{
+    unsigned long sz;
+	enum object_type type;
+	void *buf;
+
+    buf = repo_read_object_file(r, oid, &type, &sz);
+	if (!buf || type != OBJ_BLOB) {
+		free(buf);
+		return 0;
+	}
+
+    const char *guid = memmem(buf, sz, guid_prefix, prefix_length);
+    if (!guid) {
+        return 0;
+    }
+
+    guid += prefix_length;
+    if (guid + 32 > (char *)buf + sz) {
+        return 0;
+    }
+
+    memcpy(result, guid, 32);
+    return 1;
 }
--- unity.h
+++ unity.h
@@ -3,3 +3,4 @@
 */
 
 int unity_is_metafile(struct repository *r, const char *path);
+int unity_read_metafile_guid(struct repository *r, struct object_id* oid, char result[32]);
